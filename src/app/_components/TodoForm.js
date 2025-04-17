46d8c9d48f88bf05762f8655a8375c06e07b468d
Jecheon Ryu <rjc1704@gmail.com>
2025-04-17 18:43:19 +0900

    useMutation 실습준비완료

--- src/app/_components/TodoForm.js
+++ src/app/_components/TodoForm.js
@@ -1,14 +1,18 @@
 "use client";
 
+import { addTodo } from "@/api/todos";
+import { useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function TodoForm({ loadTodos }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
+
+  const queryClient = useQueryClient();
 
   const handleAddTodo = async (title) => {
     try {
       await addTodo(title);
-      // TODO: 할 일 추가 후 목록 다시 불러오기
+      await queryClient.invalidateQueries({ queryKey: ["todos"] });
     } catch (err) {
       console.error("할 일 추가 중 오류 발생:", err);
       setError("할 일을 추가하는데 실패했습니다.");
@@ -36,8 +40,12 @@ export default function TodoForm({ loadTodos }) {
           onChange={(e) => setTitle(e.target.value)}
           placeholder="할 일을 입력하세요"
           className="flex-grow p-2 border"
+          autoFocus
         />
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white">
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white cursor-pointer"
+        >
           추가
         </button>
       </div>
