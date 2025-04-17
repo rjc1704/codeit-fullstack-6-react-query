f5377ce07b521784d1261c81c0423e24425b54b6
Jecheon Ryu <rjc1704@gmail.com>
2025-04-17 23:16:32 +0900

    enabled, select 실습준비

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -15,6 +15,7 @@ export default function TodoDetailPage() {
   } = useQuery({
     queryKey: ["todos", id],
     queryFn: () => fetchTodo(id),
+    gcTime: 10 * 60 * 1000,
   });
 
   if (isPending) {
--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -5,7 +5,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function Providers({ children }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 10 * 1000,
+          },
+        },
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
