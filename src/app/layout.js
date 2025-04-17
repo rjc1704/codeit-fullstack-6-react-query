e76d40c472c6d99cac8f619c77f4f735fcad2e98
Jecheon Ryu <rjc1704@gmail.com>
2025-04-17 14:18:19 +0900

    useMutation 실습준비

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,51 +1,20 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import TodoList from "./_components/TodoList";
 import { fetchTodos } from "@/api/todos";
-
-const initialTodos = [
-  {
-    id: 1,
-    title: "할 일 1",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "할 일 2",
-    completed: true,
-  },
-  {
-    id: 3,
-    title: "할 일 3",
-    completed: false,
-  },
-];
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const {
+    data: todos,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
 
-  const loadTodos = async () => {
-    // TODO: 할 일 목록을 가져오는 로직 추가
-    // - initialTodos 제거하고 초기값 빈 배열 [] 적용
-    // - useEffect 콜백 함수 내에서 사용
-    // - fetchTodos 함수 호출
-    try {
-      const todos = await fetchTodos();
-      setTodos(todos);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    loadTodos();
-  }, []);
-
-  if (isLoading)
+  if (isPending)
     return (
       <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
     );
