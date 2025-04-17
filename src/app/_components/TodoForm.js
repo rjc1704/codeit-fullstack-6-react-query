b9637162100e45a8897931ef783888705423754d
Jecheon Ryu <rjc1704@gmail.com>
2025-04-17 22:13:55 +0900

    caching 실습준비

--- src/app/_components/TodoForm.js
+++ src/app/_components/TodoForm.js
@@ -1,46 +1,49 @@
 "use client";
 
+import { addTodo } from "@/api/todos";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function TodoForm({ loadTodos }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
 
-  const handleAddTodo = async (title) => {
-    try {
-      await addTodo(title);
-      // TODO: 할 일 추가 후 목록 다시 불러오기
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-      setError("할 일을 추가하는데 실패했습니다.");
-    }
-  };
+  const queryClient = useQueryClient();
+
+  const { mutate: mutateAddTodo, isPending: isAdding } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      console.error("할 일 추가 중 오류 발생:", error);
+      alert("할 일을 추가하는데 실패했습니다.");
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!title.trim()) return;
 
-    try {
-      await handleAddTodo(title);
-      setTitle(""); // 입력 필드 초기화
-    } catch (err) {
-      console.error("할 일 추가 중 오류 발생:", err);
-    }
+    mutateAddTodo(title);
+    setTitle("");
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mb-4">
-      <div className="flex">
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="할 일을 입력하세요"
-          className="flex-grow p-2 border"
-        />
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white">
-          추가
-        </button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="mb-4">
+        <div className="flex">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="할 일을 입력하세요"
+            className="flex-grow p-2 border"
+          />
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white">
+            {isAdding ? "추가중..." : "추가"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,15 @@
 "use client";
 
 import TodoList from "./_components/TodoList";
-import { fetchTodos } from "@/api/todos";
-import { useQuery } from "@tanstack/react-query";
-
+import TodoForm from "./_components/TodoForm";
 export default function Home() {
-  const {
-    data: todos,
-    isPending,
-    error,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
-
-  if (isPending)
-    return (
-      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
-    );
-
-  if (error)
-    return (
-      <div className="container mx-auto px-4 py-8 text-center text-red-500">
-        {error}
-      </div>
-    );
-
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-8">투두리스트</h1>
       <div className="max-w-md mx-auto mt-8">
-        {/* <TodoForm loadTodos={loadTodos} /> */}
+        <TodoForm />
         <h2 className="text-2xl font-bold mb-4">할 일 목록</h2>
-        <TodoList todos={todos} />
+        <TodoList />
       </div>
     </div>
   );
